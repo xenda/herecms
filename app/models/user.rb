45d920d19ccd4611721e54daab5856013225e758
Alvaro Pereyra Rabanal <alvaro.pereyra@srdperu.com>
2009-09-26 18:22:08 -0500

    Added user annotations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,18 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id                 :integer(4)      not null, primary key
+#  email              :string(255)
+#  encrypted_password :string(128)
+#  salt               :string(128)
+#  confirmation_token :string(128)
+#  remember_token     :string(128)
+#  email_confirmed    :boolean(1)      not null
+#  created_at         :datetime
+#  updated_at         :datetime
+#
+
 class User < ActiveRecord::Base
   include Clearance::User
 end
